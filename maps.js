70dbb3bc82fdedc5fd0342b1079568004c15f2ec
Conor Campbell <conorcampbell@live.com>
2019-12-28 07:40:55 +0100

    all information added to timeline and marker locations added

--- maps.js
+++ maps.js
@@ -31,15 +31,51 @@ function initMap() {
       },
       map: map
     });
-    map.setZoom(12);
+    map.setZoom(9);
     map.panTo(marker.position);
   })
 
-  $('#ww1').click(function() {
+  $('#fiannafail').click(function() {
     var marker = new google.maps.Marker({
       position: {
-        lat: 54.599105,
-        lng: -5.899962
+        lat: 53.3406,
+        lng: -6.2539
+      },
+      map: map
+    });
+    map.setZoom(13);
+    map.panTo(marker.position);
+  })
+
+  $('#stormont').click(function() {
+    var marker = new google.maps.Marker({
+      position: {
+        lat: 54.6040,
+        lng: -5.8328
+      },
+      map: map
+    });
+    map.setZoom(13);
+    map.panTo(marker.position);
+  })
+
+  $('#constitution').click(function() {
+    var marker = new google.maps.Marker({
+      position: {
+        lat: 53.3600,
+        lng: -6.3172
+      },
+      map: map
+    });
+    map.setZoom(10);
+    map.panTo(marker.position);
+  })
+
+  $('#blitz').click(function() {
+    var marker = new google.maps.Marker({
+      position: {
+        lat: 54.600123,
+        lng: -5.927647
       },
       map: map
     });
@@ -47,11 +83,35 @@ function initMap() {
     map.panTo(marker.position);
   })
 
-  $('#Fianna Fáil').click(function() {
+  $('#acts').click(function() {
     var marker = new google.maps.Marker({
       position: {
-        lat: 54.599105,
-        lng: -5.899962
+        lat: 51.4975,
+        lng: -0.1357
+      },
+      map: map
+    });
+    map.setZoom(7);
+    map.panTo(marker.position);
+  })
+
+  $('#civilrights').click(function() {
+    var marker = new google.maps.Marker({
+      position: {
+        lat: 54.504626,
+        lng: -6.769228
+      },
+      map: map
+    });
+    map.setZoom(11);
+    map.panTo(marker.position);
+  })
+
+  $('#bloodysunday').click(function() {
+    var marker = new google.maps.Marker({
+      position: {
+        lat: 54.996304,
+        lng: -7.326277
       },
       map: map
     });
@@ -59,29 +119,15 @@ function initMap() {
     map.panTo(marker.position);
   })
 
-  $('#ww1').click(function() {
+  $('#gfa').click(function() {
     var marker = new google.maps.Marker({
       position: {
-        lat: 54.599105,
-        lng: -5.899962
+        lat: 54.350077,
+        lng: -6.655327
       },
       map: map
     });
     map.setZoom(15);
     map.panTo(marker.position);
   })
-
-  $('#ww1').click(function() {
-    var marker = new google.maps.Marker({
-      position: {
-        lat: 54.599105,
-        lng: -5.899962
-      },
-      map: map
-    });
-    map.setZoom(15);
-    map.panTo(marker.position);
-  })
-
-
 }
